0b2688240287c7274bbf0ecd64313ca9e000dd73
Daniella Topanov <daniellatopanov@Daniellas-MacBook-Pro-2.local>
2025-08-07 21:16:37 +0300

    Fix: set host and port for Render

--- rsvp_project.py
+++ rsvp_project.py
@@ -34,7 +34,4 @@ import os
 
 
 if __name__ == '__main__':
-    import os
-    app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
